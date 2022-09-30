1bdcb287cfa33fc36d08cf90108230291e5ae4e4
s <s@USERMIC-KMLMS9F>
2022-09-30 16:09:46 +0900

    add

---
+++ chap01/src/sec06/exam01/Hello.java
@@ -0,0 +1,18 @@
+package sec06.exam01;
+
+
+/*작성자 : 김세윤
+작성일 : 2022년 9월 26일
+*/
+
+public class Hello { //클래스 블록 시작
+	
+	//메인메소드 : 프로그램 시작점 이름은 반드시 main
+	public static void main(String[] args) { //메인메소드 블록 시작
+		System.out.println("Hello World!");
+		System.out.println("Hello");
+	} //메인메소드 블록 끔
+
+} //클래스 블록 끝
+
+
