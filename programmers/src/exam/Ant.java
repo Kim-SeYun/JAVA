e71e468fb959fd1bd819300a098273bf1f2483b9
Kim-SeYun <sby06052@naver.com>
2022-10-26 14:24:45 +0900

    add

--- programmers/src/exam/Ant.java
+++ programmers/src/exam/Ant.java
@@ -2,17 +2,24 @@ package exam;
 
 public class Ant {
 	public static void main(String[] args) {
-		int hp = 23;
+		int hp = 999;
 		int result = 0;
 		int a = 5;
 		int b = 3;
 		int c = 1;
-		if(hp%a != 0) {		
-			result = hp/a;
+		if(hp%a != 0) {						
+			if((hp%a)%b ==0) {
+				result = hp/a + (hp%a)/b;
+			}else if(((hp%a)%b)%c==0) {
+				result = hp/a + (hp%a)/b + ((hp%a)%b)/c;
+			}	
 		
-		
-		}
+		}else result = hp/a;
 		System.out.println(result);
+		}
+		
 	}
 
-}
+
+	
+
