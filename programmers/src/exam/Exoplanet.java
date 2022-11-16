734928803eea5a8365e481157faed78a8fa5b9b3
Kim-SeYun <sby06052@naver.com>
2022-11-16 14:37:10 +0900

    add

--- programmers/src/exam/Exoplanet.java
+++ programmers/src/exam/Exoplanet.java
@@ -1,9 +1,22 @@
 package exam;
 
+import java.util.Arrays;
+
+
 public class Exoplanet {
 	public static void main(String[] args) {
 		int age = 23;
-		
+		String answer = "";
+
+		String str = Integer.toString(age);
+		String[] arr = str.split("");
+		System.out.println(Arrays.toString(arr));
+
+		for(int i=0; i<arr.length; i++) {
+			answer += (char)(97 + Integer.parseInt(arr[i]));
+			
+		}
+		System.out.println(answer);
 	}
 
 }
