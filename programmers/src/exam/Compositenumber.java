82850e3deafc4102b8dae79cbf02f0d8e938319a
Kim-SeYun <sby06052@naver.com>
2022-11-23 14:32:28 +0900

    add

--- programmers/src/exam/Compositenumber.java
+++ programmers/src/exam/Compositenumber.java
@@ -4,25 +4,18 @@ public class Compositenumber {
 	public static void main(String[] args) {
 		int n = 10;
 		int result = 0;
-//		for(int i=4; i<=10; i++) {
-//			if(i%2==0) {
-//				System.out.println(i);
-//			}
-//		}
-		for(int i=2; i<=n; i++) {
-			boolean isPrime = true;
-			for(int j=2; j*j<=i; j++) {
-				if(i%j==0) {
-					isPrime = false;
-					break;
-				}
-				}
-			if(isPrime) {
-				System.out.println(i + "");
-			}
+		int count = 0;
+
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i%j==0) result++;
 		}
-	
+			if(result>=3) {
+				count++;
+			}
+			result=0;
 		
 	}
-
+		System.out.println(count);
+	}
 }
