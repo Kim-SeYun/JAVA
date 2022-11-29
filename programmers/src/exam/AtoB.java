bc8a7312443a0ade9fb758b6ec30c488914a9c9b
Kim-SeYun <sby06052@naver.com>
2022-11-29 14:15:06 +0900

    add

--- programmers/src/exam/AtoB.java
+++ programmers/src/exam/AtoB.java
@@ -1,10 +1,21 @@
 package exam;
 
+import java.util.Arrays;
+
 public class AtoB {
 	public static void main(String[] args) {
 		
 		String before = "olleh";
 		String after = "hello";
-	
+		int a =0;
+		int b=0;
+		int result = 0;
+
+		for(int i=0; i<before.length(); i++) {
+			a += before.charAt(i);
+			b += after.charAt(i);
+			result = (a==b)?1:0;
+		}
+		System.out.println(result);
     }
 }
