41725bf24527dc4ae7bc7975d35210b903eb84de
Kim-SeYun <sby06052@naver.com>
2022-11-30 14:25:47 +0900

    add

---
+++ programmers/src/exam/CloseNumber.java
@@ -0,0 +1,9 @@
+package exam;
+
+public class CloseNumber {
+	public static void main(String[] args) {
+		int[] array = {3, 10, 28};
+		int n = 20;
+	}
+
+}
--- programmers/src/exam/Factorial.java
+++ programmers/src/exam/Factorial.java
@@ -2,14 +2,25 @@ package exam;
 
 public class Factorial {
 	public static void main(String[] args) {
-		int n = 10;
+		int n = 7;
 		int result = 1;
 		
-		for(int i=n; i>0; i--) {
-			result *= i;
-		}
-		System.out.println(result);
+		// 팩토리얼 구하는 코드
+//		for(int i=n; i>0; i--) {
+//			result *= i;
+//		}
+//		System.out.println(result);
 		
+		for(int i=1; i>0; i++) {
+			result *= i;
+			
+			if(result==n) {
+				System.out.println(i);
+			}else if(result>n) {
+				System.out.println(i-1);
+				break;
+			}
+		}
 	}
 
 }
