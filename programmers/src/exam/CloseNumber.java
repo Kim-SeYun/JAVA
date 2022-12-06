a8e24b55450db0302d9fa962003275ae6964cf2c
Kim-SeYun <sby06052@naver.com>
2022-12-06 11:26:53 +0900

    add

--- programmers/src/exam/CloseNumber.java
+++ programmers/src/exam/CloseNumber.java
@@ -1,9 +1,21 @@
 package exam;
 
+import java.util.Arrays;
+
 public class CloseNumber {
 	public static void main(String[] args) {
 		int[] array = {3, 10, 28};
 		int n = 20;
+		int answer = 0;
+		int[] aa = new int[array.length];
+		
+		for(int i=0; i<array.length; i++) {
+			answer = Math.abs(array[i]-n);
+			System.out.println(answer);
+			aa[i] =answer;
+			
+		}
+		System.out.println(Arrays.toString(aa));
 	}
 
 }
