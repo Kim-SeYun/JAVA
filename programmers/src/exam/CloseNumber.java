f3e238cb78ac6d61d37deaff8dd054723c3f5c7f
Kim-SeYun <sby06052@naver.com>
2022-12-06 15:26:02 +0900

    add

--- programmers/src/exam/CloseNumber.java
+++ programmers/src/exam/CloseNumber.java
@@ -4,18 +4,24 @@ import java.util.Arrays;
 
 public class CloseNumber {
 	public static void main(String[] args) {
-		int[] array = {3, 10, 28};
+		int[] array = {3, 28, 12};
 		int n = 20;
 		int answer = 0;
-		int[] aa = new int[array.length];
+		int min = Math.abs(array[0]-n);
+		int minIndex = 0;
+		Arrays.sort(array);
 		
 		for(int i=0; i<array.length; i++) {
-			answer = Math.abs(array[i]-n);
-			System.out.println(answer);
-			aa[i] =answer;
+			if( Math.abs(array[i]-n)< min){
+				min = Math.abs(array[i]-n);
+				minIndex = i;
+			}
 			
-		}
-		System.out.println(Arrays.toString(aa));
+			}
+		System.out.println(min);
+		System.out.println(minIndex);
+		System.out.println(array[minIndex]);
+		
 	}
 
 }
